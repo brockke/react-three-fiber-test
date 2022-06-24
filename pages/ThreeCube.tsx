7b5806332b65cc14f4d73bacecdfda445f653254
brockke <brock97k@gmail.com>
2022-06-24 13:02:12 -0300

    created a basic rotating cube

--- pages/ThreeCube.tsx
+++ pages/ThreeCube.tsx
@@ -1,13 +1,27 @@
-import { Canvas } from '@react-three/fiber'
+import { useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Mesh } from 'three'
+
+const Stars = () => {
+  const myMesh = useRef<Mesh>(null!)
+  useFrame(({clock}, delta) => {
+    // console.log(delta)
+    myMesh.current.rotation.x -= delta / 10
+    myMesh.current.rotation.y -= delta / 15
+  })
+  return(  
+    <mesh ref={myMesh}>
+      <boxGeometry args={[2, 2, 2]}/>
+      <meshStandardMaterial />
+    </mesh>
+  );
+}
 
 const ThreeCube = () => (
     <Canvas>
       <ambientLight intensity={0.1} />
       <directionalLight color="red" position={[0, 0, 5]} />
-      <mesh>
-        <boxGeometry args={[2, 2, 2]}/>
-        <meshStandardMaterial />
-      </mesh>
+      <Stars />
     </Canvas>
 );
 
