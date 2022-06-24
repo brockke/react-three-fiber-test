4f492fca5d9922401dfcf8de1a18f5cf458dc47a
brockke <brock97k@gmail.com>
2022-06-24 09:47:49 -0300

    added react-three-fiber to project

---
+++ pages/ThreeCube.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from 'react-dom'
+import { Canvas } from '@react-three/fiber'
+
+const ThreeCube = () => (
+  <div id="canvas-container">
+    <Canvas>
+      
+    </Canvas>
+  </div>
+);
+
+export { ThreeCube };
+// ReactDOM.render(<App />, document.getElementById('root'))
